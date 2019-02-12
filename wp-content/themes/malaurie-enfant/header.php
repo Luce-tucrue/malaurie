944d7e41721cd3bfbf568ade2443a3a894fd554a
Lucile <lucile.fromant@capgemini.com>
2019-02-12 09:40:33 +0100

    somes updates

--- wp-content/themes/malaurie-enfant/header.php
+++ wp-content/themes/malaurie-enfant/header.php
@@ -13,57 +13,81 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-  <meta charset="<?php bloginfo('charset'); ?>">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="profile" href="https://gmpg.org/xfn/11">
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
 
-  <?php wp_head(); ?>
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-  <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e('Skip to content', 'malaurie'); ?></a>
+    <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e('Skip to content', 'malaurie'); ?></a>
 
-  <header class="header" role="banner">
-    <div class="header__branding">
-      <div class="header__branding__logo">
-        <?php the_custom_logo(); ?>
-      </div>
-      <h1 class="header__branding__title">
-        <a href="<?php echo esc_url(home_url('/')); ?>" rel="home"  class="header__branding__title__link">
-          <span class="header__branding__title__link__name"><?php bloginfo('name'); ?></span>
+    <header class="header" role="banner">
+        <div class="header__branding">
+            <div class="header__branding__logo">
+                <?php the_custom_logo(); ?>
+            </div>
+            <h1 class="header__branding__title">
+                <a href="<?php echo esc_url(home_url('/')); ?>" rel="home" class="header__branding__title__link">
+                    <span class="header__branding__title__link__name"><?php bloginfo('name'); ?></span>
 
-          <?php $malaurie_description = get_bloginfo('description', 'display'); ?>
-          <span class="header__branding__title__link__description"><?php echo $malaurie_description; /* WPCS: xss ok. */ ?></span>
+                    <?php $malaurie_description = get_bloginfo('description', 'display'); ?>
+                    <span class="header__branding__title__link__description"><?php echo $malaurie_description; /* WPCS: xss ok. */ ?></span>
 
-        </a>
-      </h1>
-
-
-    </div><!-- .site-branding -->
-
-    <nav id="site-navigation" class="navbar" role="navigation">
-      <div class="container">
-        <button class="menu-toggle" aria-controls="primary-menu"
-                aria-expanded="false"><?php esc_html_e('Primary Menu', 'malaurie'); ?></button>
-        <div class="navigation">
-
-            <?php
-            if ( get_page_template_slug( get_the_ID('2') ) ){
-                wp_nav_menu( array( 'theme_location' => 'menu-1','menu' => 'navigation','walker' => new mono_walker() ) );
-
-            } else {
-                wp_nav_menu( array( 'theme_location' => 'menu-1','menu' => 'navigation'));
-            }
-            ?>
+                </a>
+            </h1>
 
 
-        </div>
-      </div>
-    </nav><!-- #site-navigation -->
-  </header><!-- #masthead -->
+        </div><!-- .site-branding -->
 
-  <div id="content" class="site-content">
+        <nav id="site-navigation" class="navbar" role="navigation">
+            <div class="container">
+
+                <div class="navigation">
+
+                    <div class="d-lg-none d-xl-none">
+                        <button class="" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1" aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"> Menu </span></button>
+
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent1">
+                            <?php
+                            if (get_page_template_slug(get_the_ID('2'))) {
+                                wp_nav_menu(array(
+                                    'theme_location' => 'menu-1',
+                                    'menu' => 'navigation',
+                                    'menu_class' => 'nav-menu',
+                                    'walker' => new mono_walker()));
+
+                            } else {
+                                wp_nav_menu(array('theme_location' => 'menu-1', 'menu' => 'navigation', 'menu_class' => 'nav-menu'));
+                            }
+                            ?>
+                        </div>
+
+                    </div>
+
+                    <div class="d-none d-lg-block d-xl-block">
+                        <?php
+                        if (get_page_template_slug(get_the_ID('2'))) {
+                            wp_nav_menu(array(
+                                'theme_location' => 'menu-1',
+                                'menu' => 'navigation',
+                                'menu_class' => 'nav-menu',
+                                'walker' => new mono_walker()));
+
+                        } else {
+                            wp_nav_menu(array('theme_location' => 'menu-1', 'menu' => 'navigation', 'menu_class' => 'nav-menu'));
+                        }
+                        ?>
+                    </div>
+
+                </div>
+            </div>
+        </nav><!-- #site-navigation -->
+    </header><!-- #masthead -->
+
+    <div id="content" class="site-content">
 
 
 
